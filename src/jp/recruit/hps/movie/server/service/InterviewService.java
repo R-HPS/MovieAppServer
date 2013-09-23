15a698df3c898b74347e8e4d222b37d56f3d7325
ippei-takahashi <aimluck.ippei.takahashi@gmail.com>
2013-09-23 17:53:22 +0900

    a

--- src/jp/recruit/hps/movie/server/service/InterviewService.java
+++ src/jp/recruit/hps/movie/server/service/InterviewService.java
@@ -6,9 +6,10 @@ import java.util.List;
 import java.util.Map;
 
 import jp.recruit.hps.movie.server.meta.InterviewMeta;
-import jp.recruit.hps.movie.server.model.Company;
 import jp.recruit.hps.movie.server.model.Interview;
+import jp.recruit.hps.movie.server.model.Interview.Atmosphere;
 import jp.recruit.hps.movie.server.model.Interview.Category;
+import jp.recruit.hps.movie.server.model.InterviewGroup;
 import jp.recruit.hps.movie.server.model.User;
 
 import org.slim3.datastore.Datastore;
@@ -21,20 +22,20 @@ public class InterviewService {
     private static InterviewMeta meta = InterviewMeta.get();
 
     public static Interview createInterview(Map<String, Object> input,
-            User user, Company company) {
+            User user, InterviewGroup interviewGroup) {
         Interview interview = new Interview();
         Key key = Datastore.allocateId(Interview.class);
         BeanUtil.copy(input, interview);
         interview.setKey(key);
         interview.getUserRef().setModel(user);
-        interview.getCompanyRef().setModel(company);
+        interview.getInterviewGroupRef().setModel(interviewGroup);
         Transaction tx = Datastore.beginTransaction();
         Datastore.put(interview);
         tx.commit();
         return interview;
     }
 
-    public static Interview createInterview(User user, Company company,
+    public static Interview createInterview(User user, InterviewGroup interviewGroup,
             Date startDate, Date endDate, String question, int atmosphere,
             Category category) {
         Map<String, Object> map = new HashMap<String, Object>();
@@ -43,24 +44,32 @@ public class InterviewService {
         map.put("question", question);
         map.put("atomosphere", atmosphere);
         map.put("category", category);
-        return createInterview(map, user, company);
+        return createInterview(map, user, interviewGroup);
     }
 
-    public static Interview createInterview(User user, Company company,
-            Date startDate, Date endDate, String question) {
+    public static Interview createInterview(User user, InterviewGroup interviewGroup,
+            Date startDate) {
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("startDate", startDate);
-        map.put("endDate", endDate);
-        map.put("question", question);
-        return createInterview(map, user, company);
+        return createInterview(map, user, interviewGroup);
+    }
+    
+    public static void updateInterview(Interview interview,
+            Date startDate, Date endDate, String question, Atmosphere atmosphere,
+            Category category) {
+        interview.setStartDate(startDate);
+        interview.setEndDate(endDate);
+        interview.setQuestion(question);
+        interview.setAtmosphere(atmosphere);
+        interview.setCategory(category);
     }
 
-    public static List<Interview> getInterviewListByCompanyKey(Key key) {
+    public static List<Interview> getInterviewListByInterviewGroupKey(Key key) {
         try {
             return Datastore
                 .query(meta)
-                .filter(meta.companyRef.equal(key))
-                //.sort(meta.startDate.asc)
+                .filter(meta.interviewGroupRef.equal(key))
+                .sort(meta.startDate.asc)
                 .asList();
         } catch (Exception e) {
             return null;
@@ -78,12 +87,12 @@ public class InterviewService {
         }
     }
 
-    public static int getInterviewCount(Key userKey, Key companyKey) {
+    public static int getInterviewCount(Key userKey, Key interviewGroupKey) {
         return Datastore
             .query(meta)
             .filter(
                 meta.userRef.equal(userKey),
-                meta.companyRef.equal(companyKey))
+                meta.interviewGroupRef.equal(interviewGroupKey))
             .count();
     }
 }
