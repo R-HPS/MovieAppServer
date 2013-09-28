db34d7dd10c3c5309cc5cabe03ee7a2e43c31072
ippei-takahashi <aimluck.ippei.takahashi@gmail.com>
2013-09-28 18:08:32 +0900

    archive

---
+++ src/jp/recruit/hps/movie/server/api/dto/ArchiveV1Dto.java
@@ -0,0 +1,38 @@
+package jp.recruit.hps.movie.server.api.dto;
+
+import javax.persistence.Entity;
+
+@Entity
+public class ArchiveV1Dto {
+    private String type;
+    
+    private int year;
+    
+    private String body;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+   
+
+}
